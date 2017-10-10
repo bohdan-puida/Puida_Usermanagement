8d3fffc048f73fd951d736d88db4670a98712288
bohdan-puida <bogdanpuida529@gmail.com>
2017-10-10 23:21:45 +0300

    Another attempt from idea

--- src/main/java/cs5/puida/User.java
+++ src/main/java/cs5/puida/User.java
@@ -41,7 +41,8 @@ public class User {
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
-    
+
+
 
     public String getFullName() throws IllegalStateException {
         if (getFirstName() == null || getLastName() == null) {
